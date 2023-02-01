98670406ec26c553a3d88e58501186bab60b2e1d
SATalukder <shajibahmad152@gmail.com>
2023-02-01 18:00:16 +0600

    for of loop

---
+++ js-core-concepts-bonus/match.js
@@ -0,0 +1,29 @@
+const numbers = [45, 65, 23, 98, 19];
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    // console.log(number);
+}
+
+//Use For Of loop
+
+for (const number of numbers) {
+    // console.log(number);
+
+}
+
+const products = [
+    { id: 1, name: 'xiami phone one night', price: 19000 },
+    { id: 2, name: 'iphone', price: 19000 },
+    { id: 3, name: 'mac book air', price: 119000 },
+    { id: 4, name: 'lenovo yoga laptop 2025', price: 19000 },
+    { id: 5, name: 'Dell inspiron laptop', price: 19000 },
+    { id: 6, name: 'samsung phone note 7', price: 19000 },
+    { id: 7, name: 'Nokia old phone gone', price: 19000 },
+    { id: 8, name: 'phone one', price: 19000 },
+
+]
+
+for (const product of products) {
+    console.log(product);
+}
